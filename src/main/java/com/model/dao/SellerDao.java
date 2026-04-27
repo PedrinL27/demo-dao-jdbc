a36a7195c802768a52ebb6caeeabffbdbceb66a6
PedrinL27 <pedrolangelineto0@gmail.com>
2026-04-26 21:34:47 -0300

    Added Dao of the seller and department

---
+++ src/main/java/com/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package com.model.dao;
+
+import java.util.List;
+
+import com.model.entities.Seller;
+
+public interface SellerDao {
+
+    void insert(Seller obj);
+    void update(Seller obj);
+    void delete(Seller obj);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
